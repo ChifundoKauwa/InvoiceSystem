45dce547947e1f17a66a6aa3a9cff0e1d1429cca
ChifundoKauwa <bsc-com-15-21@unima.ac.mw>
2025-12-25 19:25:37 +0200

    domain: implement invoice domain events (Issued, Paid, Overdue, Voided)

--- invoice/InvoiceEvent.ts
+++ invoice/InvoiceEvent.ts
@@ -1,22 +1,24 @@
-class InvoiceIssued extends DomainEvent {
+import { DomainEvent } from "./DomainEvent";
+
+export class InvoiceIssued extends DomainEvent {
     constructor(public readonly invoiceId: string, public readonly issuedAt: Date, public readonly dueAt: Date  ) 
     {
         super();
     }
 }
-class InvoicePaid extends DomainEvent {
+export class InvoicePaid extends DomainEvent {
     constructor(public readonly invoiceId: string, public readonly paidAt: Date) 
     {
         super();
     }
 }
-class InvoiceOverdue extends DomainEvent {
+export class InvoiceOverdue extends DomainEvent {
     constructor(public readonly invoiceId: string, public readonly overdueAt: Date) 
     {
         super();
     }   
 }
-class InvoiceVoided extends DomainEvent {
+export class InvoiceVoided extends DomainEvent {
     constructor(public readonly invoiceId: string, public readonly voidedAt: Date) 
     {
         super();
